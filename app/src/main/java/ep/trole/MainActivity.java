432ba30bc9d800c067ddac52f12f8a3e0c8231dc
David Jelenc <david.jelenc@fri.uni-lj.si>
2016-12-12 21:34:34 +0100

    Retrofit solution

--- app/src/main/java/ep/trole/MainActivity.java
+++ app/src/main/java/ep/trole/MainActivity.java
@@ -8,17 +8,41 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
-    public static final String ADDRESS_BOTH = "https://www.trola.si/%s/%s/";
-    public static final String ADDRESS_SINGLE = "https://www.trola.si/%s/";
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Path;
 
+
+/**
+ * Ta rešitev uporablja zunanje knjižnice.
+ * <p>
+ * Knjižnico Retrofit2 se uporabi za podajanje zahtevkov HTTP.
+ * URL http://square.github.io/retrofit
+ * <p>
+ * Strežnikov odgovor (v formatu JSON) se avtomatično razčleni in pretvori v Javanske objekte
+ * s pomočjo knjižnic GSON ter converter-gson.
+ */
+public class MainActivity extends AppCompatActivity implements Callback<MainActivity.TrolaResponse> {
     private static final String TAG = MainActivity.class.getCanonicalName();
 
     private EditText station;
     private EditText line;
     private TextView arrivals;
     private Button searchButton;
+
+    private final Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://www.trola.si/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    private final TrolaService service = retrofit.create(TrolaService.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +56,69 @@ public class MainActivity extends AppCompatActivity {
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String currentStation = station.getText().toString().trim();
-                final String currentLine = line.getText().toString().trim();
-                arrivals.setText(currentStation + " : " + currentLine);
-                Log.i(TAG, "Poizvedba za linijo " + currentLine + " in postajo " + currentStation);
+                final String station = MainActivity.this.station.getText().toString().trim();
+                final String line = MainActivity.this.line.getText().toString().trim();
 
-                // TODO: Implementirajte poizvedbo o prihodu avtobusev
+                if (!station.isEmpty() && !line.isEmpty()) {
+                    final Call<TrolaResponse> rides = service.getRides(station, line);
+                    rides.enqueue(MainActivity.this);
+                } else if (!station.isEmpty()) {
+                    final Call<TrolaResponse> rides = service.getRides(station);
+                    rides.enqueue(MainActivity.this);
+                }
             }
         });
+    }
+
+    @Override
+    public void onResponse(Call<TrolaResponse> call, Response<TrolaResponse> response) {
+        final List<Station> stations = response.body().stations;
+
+        if (stations == null || stations.isEmpty()) {
+            arrivals.setText(R.string.no_hits);
+            return;
+        }
+
+        final StringBuilder sb = new StringBuilder();
+        for (Station s : stations) {
+            sb.append(s.name).append(" (").append(s.number).append("):\n");
+            for (Bus b : s.buses) {
+                if (!b.arrivals.isEmpty()) {
+                    sb.append("  ").append(b.number).append("-")
+                            .append(b.direction).append(": ")
+                            .append(arrivals).append("\n");
+                }
+            }
+        }
+        arrivals.setText(sb.toString());
+    }
+
+    @Override
+    public void onFailure(Call<TrolaResponse> call, Throwable t) {
+        Log.wtf(TAG, "Failure: " + t.getMessage(), t);
+    }
+
+    interface TrolaService {
+        @Headers("accept: application/json")
+        @GET("{station}/")
+        Call<TrolaResponse> getRides(@Path("station") String station);
+
+        @Headers("accept: application/json")
+        @GET("{station}/{line}/")
+        Call<TrolaResponse> getRides(@Path("station") String station, @Path("line") String line);
+    }
+
+    static class TrolaResponse {
+        List<Station> stations;
+    }
+
+    static class Station {
+        String number, name;
+        List<Bus> buses;
+    }
+
+    static class Bus {
+        String direction, number;
+        List<Integer> arrivals;
     }
 }
